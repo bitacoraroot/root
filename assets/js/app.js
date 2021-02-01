47aa8afb5d9f7034e12c13a7b333a2b0535abb82
Bitacora de root <36718648+bitacoraroot@users.noreply.github.com>
2021-01-31 23:40:03 -0400

    Update data
    Update my data

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,12 +4,15 @@ $(function() {
     	transition: [ 'fade', 'zoomOut', 'slideUp' ],
     	overlay: true,
 	    slides: [
-	    { src: "assets/images/slide/bg1.png" },
-	    { src: "assets/images/slide/bg4.jpg" },
-		{ src: "assets/images/slide/bg5.jpg" },
-		{ src: "assets/images/slide/bg6.jpg" },
-        { src: "assets/images/slide/bg7.jpg" },
-	    { src: "assets/images/slide/bg8.jpg" }
+	    { src: "assets/images/slide/bg8.jpg" },
+		{ src: "assets/images/slide/bg9.jpg" },
+	    { src: "assets/images/slide/bg2.jpg" },
+		{ src: "assets/images/slide/bg3.jpg" },
+		{ src: "assets/images/slide/bg4.jpg" },
+        { src: "assets/images/slide/bg5.jpg" },
+		{ src: "assets/images/slide/bg6.jpg" }
+    // { src: "assets/images/slide/bg7.png" },
+	// { src: "assets/images/slide/bg1.jpg" }
 	    ]
 	});
 });
